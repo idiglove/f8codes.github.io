bd1a119469dec07997f1382dfafa816a1fef0fb3
Faith Morante <faith.morante@gmail.com>
2019-02-16 22:22:47 +0800

    Updates

---
+++ precache-manifest.a1a36f994457b59177370df46016bc5b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "c113264895a1db1e074b58440a3726fa",
+    "url": "/static/media/2.c1132648.jpg"
+  },
+  {
+    "revision": "b1bef750e15e728c9701",
+    "url": "/static/css/main.c6efec38.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "adfbe21aabc1618c5a9a602adcb9f621",
+    "url": "/static/media/phingsdesserts2.adfbe21a.jpg"
+  },
+  {
+    "revision": "f84d22094e953bfc639e",
+    "url": "/static/js/2.f84d2209.chunk.js"
+  },
+  {
+    "revision": "4da0634fa5040d6c8ccf7283b5a716b9",
+    "url": "/static/media/photo-edited.4da0634f.jpg"
+  },
+  {
+    "revision": "b970185a6eba65f4d10f52749df0ac9c",
+    "url": "/static/media/1.b970185a.jpg"
+  },
+  {
+    "revision": "b1bef750e15e728c9701",
+    "url": "/static/js/main.b1bef750.chunk.js"
+  },
+  {
+    "revision": "f06ea1d4a61d30270b98b8d837d1501d",
+    "url": "/static/media/3.f06ea1d4.jpg"
+  },
+  {
+    "revision": "d0808817bd796923118f69bd6ead8897",
+    "url": "/static/media/4.d0808817.jpg"
+  },
+  {
+    "revision": "9103b1692269ccc9c777ce1316049d2a",
+    "url": "/static/media/juniortvflyer2.9103b169.jpg"
+  },
+  {
+    "revision": "6a28ee6ff684c7708e34db563f45b599",
+    "url": "/static/media/bullwingsmenucover.6a28ee6f.jpg"
+  },
+  {
+    "revision": "bf43dfab132f6dad060184b0e4c1b03b",
+    "url": "/static/media/bullwingspage.bf43dfab.jpg"
+  },
+  {
+    "revision": "f84d22094e953bfc639e",
+    "url": "/static/css/2.d8998eb6.chunk.css"
+  },
+  {
+    "revision": "f57929c19314bf0c1ccb062166afc716",
+    "url": "/index.html"
+  }
+];
