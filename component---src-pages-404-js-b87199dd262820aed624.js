5078886babcbbaea876e58235832a1b8574bb994
Faith <faith.morante@honcho.com>
2019-02-18 08:53:13 +0800

    Updates

---
+++ component---src-pages-404-js-b87199dd262820aed624.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{142:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a);t.default=function(){return u.a.createElement(Layout,null,u.a.createElement(SEO,{title:"404: Not found"}),u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-b87199dd262820aed624.js.map
