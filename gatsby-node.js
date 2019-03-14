1e01a23591a7d71e23e7c40cd55143f8dba89a3f
Faith <faith.morante@honcho.com>
2019-03-14 14:06:08 +0800

    add pagination

--- gatsby-node.js
+++ gatsby-node.js
@@ -50,5 +50,22 @@ exports.createPages = ({ actions, graphql }) => {
         context: {}, // additional data can be passed via context
       })
     })
+
+    const posts = result.data.allFile.edges
+    const postsPerPage = 3;
+    const numPages = Math.ceil(posts.length / postsPerPage);
+
+    Array.from({ length: numPages }).forEach((_, i) => {
+      createPage({
+        path: i === 0 ? `/blog` : `/blog/${i + 1}`,
+        component: path.resolve('./src/pages/blog.js'),
+        context: {
+          limit: postsPerPage,
+          skip: i * postsPerPage,
+          numPages,
+          currentPage: i + 1
+        },
+      });
+    });
   })
 }
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,54 +1,78 @@
-import React from "react"
+import React, { Component } from "react"
 import Link from "gatsby-link"
 import Header from './header'
 import './../css/style.css'
 import '../fonts/fonts.css';
 import { Nav, Navbar, Container, Row, Col } from 'react-bootstrap';
 
-export default ({ data }) => {
-    return (
-        <div>
+// export default ({ data }) => {
+class Blog extends Component {   
+    render () {
+        const {data} = this.props
+        const { currentPage, numPages } = this.props.pageContext
+        const isFirst = currentPage === 1
+        const isLast = currentPage === numPages
+        const prevPage = currentPage - 1 === 1 ? "/blog" : '/blog/' + (currentPage - 1).toString()
+        const nextPage = '/blog/' + (currentPage + 1).toString()
 
-            <Header/>
+        return (
+            <div>
 
-            <div className="content blog-content">
-                <h1 className="main-title">
-                    Faith Blogs
-                </h1>
-                <h4 className="post-count">{data.allMarkdownRemark.totalCount} Posts</h4>
-                <Container>
-                    <Row>
-                        {data.allMarkdownRemark.edges.map(({ node }) => (
-                            <Col sm={4} className="blog-post">
-                                <div key={node.id}>
-                                    <Link
-                                        to={node.frontmatter.path}
-                                        css={{ textDecoration: `none`, color: `inherit` }}
-                                        className="blog-title"
-                                    >
-                                        <img src={node.frontmatter.thumbnail} />
-                                        <h3 style={{ marginBottom: '4px' }}>
-                                            {node.frontmatter.title}{" "}
-                                            <span className="blog-date">— {node.frontmatter.date}</span>
-                                        </h3>
-                                    </Link>
-                                    <p>{node.excerpt}</p>
-                                </div>
-                            </Col>
-                        ))}
-                    </Row>
-                </Container>
+                <Header/>
+
+                <div className="content blog-content">
+                    <h1 className="main-title">
+                        Faith Blogs
+                    </h1>
+                    <h4 className="post-count">{data.allMarkdownRemark.totalCount} Posts</h4>
+                    <Container>
+                        <Row>
+                            {data.allMarkdownRemark.edges.map(({ node }) => (
+                                <Col sm={4} className="blog-post">
+                                    <div key={node.id}>
+                                        <Link
+                                            to={node.frontmatter.path}
+                                            css={{ textDecoration: `none`, color: `inherit` }}
+                                            className="blog-title"
+                                        >
+                                            <img src={node.frontmatter.thumbnail} />
+                                            <h3 style={{ marginBottom: '4px' }}>
+                                                {node.frontmatter.title}{" "}
+                                                <span className="blog-date">— {node.frontmatter.date}</span>
+                                            </h3>
+                                        </Link>
+                                        <p>{node.excerpt}</p>
+                                    </div>
+                                </Col>
+                            ))}
+                        </Row>
+                    </Container>
+                    {!isFirst && (
+                        <Link to={prevPage} rel="prev" className="pagination-link">
+                        ← Previous Page
+                        </Link>
+                    )}
+                    {!isLast && (
+                        <Link to={nextPage} rel="next" className="pagination-link">
+                        Next Page →
+                        </Link>
+                    )}
+                </div>
             </div>
-
-        </div>
-    );
+        );
+    }   
 };
 
+export default Blog
+
 export const query = graphql`
-query IndexQuery {
+query($skip: Int, $limit: Int) { 
     allMarkdownRemark(
-        sort: {fields: [frontmatter___date], order: DESC}, 
-        filter: {frontmatter: {path: {regex: "/^\/blog/"}}}) {
+            sort: {fields: [frontmatter___date], order: DESC}, 
+            filter: {frontmatter: {path: {regex: "/^\/blog/"}}},
+            limit: $limit
+            skip: $skip
+        ) {
       totalCount
       edges {
         node {
