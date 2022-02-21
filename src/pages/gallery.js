bf97908e9e1977ddb9b8ef1d47c210ec960c6233
Faith Morante <faith.morante@gmail.com>
2022-02-21 21:53:56 +0800

    fix build errors

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,53 +1,49 @@
-import React from "react"
-import Link from "gatsby-link"
-import Header from './header'
-import './../css/style.css'
-import '../fonts/fonts.css';
-import { Container, Row, Col } from 'react-bootstrap';
+import React from "react";
+import { graphql } from "gatsby";
+import Header from "./header";
+import "./../css/style.css";
+import "../fonts/fonts.css";
+import { Container, Row, Col } from "react-bootstrap";
 
 export default ({ data }) => {
-    return (
-        <div>
+  return (
+    <div>
+      <Header />
 
-            <Header/>
-
-            <div className="content">
-                <h1 className="main-title">
-                    Gallery
-                </h1>
-                <Container>
-                  <Row>
-                  {data.allFile.edges.map(({ node }) => (
-                    <Col sm={4} className="gallery-img">
-                      <div key={node.childMarkdownRemark.id}>
-                          <img src={node.childMarkdownRemark.frontmatter.image}></img>
-                      </div>
-                    </Col>
-                  ))}
-                  </Row>
-                </Container>
-            </div>
-
-        </div>
-    );
+      <div className="content">
+        <h1 className="main-title">Gallery</h1>
+        <Container>
+          <Row>
+            {data.allFile.edges.map(({ node }) => (
+              <Col sm={4} className="gallery-img">
+                <div key={node.childMarkdownRemark.id}>
+                  <img src={node.childMarkdownRemark.frontmatter.image}></img>
+                </div>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
 };
 
 export const query = graphql`
-    query galleryQuery {
-      allFile(filter: {sourceInstanceName: {eq: "gallery"}}) {
-        totalCount
-        edges {
-          node {
-            childMarkdownRemark {
-              id
-              excerpt
-              frontmatter {
-                title
-                image
-              }
+  query galleryQuery {
+    allFile(filter: { sourceInstanceName: { eq: "gallery" } }) {
+      totalCount
+      edges {
+        node {
+          childMarkdownRemark {
+            id
+            excerpt
+            frontmatter {
+              title
+              image
             }
           }
         }
       }
     }
+  }
 `;
